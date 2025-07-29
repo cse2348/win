587e58f45a8d17e298a6835dec0c794bdadff32e
seoyeon <2371013@ewhain.net>
2025-07-30 00:10:22 +0900

    정치뉴스 - 매우쉬움

--- src/main/java/com/example/win/entity/DataLoader.java
+++ src/main/java/com/example/win/entity/DataLoader.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
 @Component
 @RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
