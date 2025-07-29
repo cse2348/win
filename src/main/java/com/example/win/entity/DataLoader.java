5457af6efe0872a54dee67b32a0edaecdee8fdc7
seoyeon <2371013@ewhain.net>
2025-07-30 00:43:41 +0900

    정치뉴스 - 매우쉬움

--- src/main/java/com/example/win/entity/DataLoader.java
+++ src/main/java/com/example/win/entity/DataLoader.java
@@ -14,6 +14,7 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
         excelNewsService.loadExcelDataToDb("data/politics.xlsx");
     }
 }
