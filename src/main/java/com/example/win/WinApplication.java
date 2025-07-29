e6a3c9d4459e46447665c2288c743a2435ae29ed
seoyeon <2371013@ewhain.net>
2025-07-29 16:58:51 +0900

    news commit

--- src/main/java/com/example/win/WinApplication.java
+++ src/main/java/com/example/win/WinApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WinApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WinApplication.class, args);
 	}
-
 }
